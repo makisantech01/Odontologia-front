a8f92657816ca3a6f1323a07c8949b2080a9b28f
Gxspxr <sernagas1234@gmail.com>
2023-07-10 23:56:52 -0300

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Login from "./views/Login";
 import Citas from "./views/Citas";
-import Asdasd from "./components/Inventory/Inventory";
+import Inventario from "./components/Inventory/Inventory";
 import Pacientes from "./views/Pacientes";
 import Register from "./views/Register";
 
@@ -9,11 +9,12 @@ function App() {
   return (
       <Routes>
         <Route path="/citas" element={<Citas />} />
-        <Route path="/inventario" element={<Asdasd />} />
+        <Route path="/inventario" element={<Inventario />} />
         <Route path="/pacientes" element={<Pacientes />} />
         <Route path="/registrarse" element={<Register />} />
         <Route path="/" element={<Login />} />
       </Routes>
+
   );
 }
 
