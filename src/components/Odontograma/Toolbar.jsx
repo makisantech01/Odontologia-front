d7fb48835010960c155b786af5549fa49160030b
CristopherAreche <cristopherareche764@gmail.com>
2023-10-17 11:37:11 -0400

    commit

--- src/components/Odontograma/Toolbar.jsx
+++ src/components/Odontograma/Toolbar.jsx
@@ -4,35 +4,37 @@ import "react-awesome-button/dist/styles.css";
 const Toolbar = (props) => {
   const [selectedButton, setSelectedButton] = useState(null);
 
-  const handleButtonClick = (cor, nome) => {
+  const handleButtonClick = (colors, nome) => {
     if (selectedButton === nome) {
       setSelectedButton(null);
     } else {
       setSelectedButton(nome);
-      props.handleAction(cor, nome);
+      props.handleAction(colors, nome);
     }
   };
 
   return (
     <div className="flex flex-wrap gap-2 w-full justify-evenly py-3">
       {props.toolbar.opcoes.map((item) => {
-        let cor = item.cor;
+        let colors = item.cor;
         let nome = item.nome;
+        console.log("colores -->", colors);
 
-        const buttonStyle = {
-          backgroundColor:
-            selectedButton === nome ? `bg-${cor}-500` : `bg-${cor}-800`,
-        };
+        // const buttonStyle = {
+        //   backgroundColor:
+        //     selectedButton === nome ? `bg-${colors}-700` : `bg-${colors}-800`,
+        // };
 
         return (
           <button
             type="primary"
             key={nome}
-            className={`bt-toolbar bg-${cor}-500 py-2 px-3 rounded-lg`}
-            style={buttonStyle}
-            onClick={() => handleButtonClick(cor, nome)}
+            className={`bg-gray-400 py-2 px-3 rounded-lg flex flex-row gap-3 items-center`}
+            // style={buttonStyle}
+            onClick={() => handleButtonClick(colors, nome)}
           >
-            {nome}
+            <p>{nome}</p>
+            <p className={`rounded-full p-2 bg-${colors}-700`}></p>
           </button>
         );
       })}
